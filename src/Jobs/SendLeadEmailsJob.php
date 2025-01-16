2140be4340d28030d3059deb8caae6eeda3fb67b
Owen Connor <owen.connor@techquity.co.uk>
2025-01-16 16:27:18 +0000

    Update SendLeadEmailsJob.php

--- src/Jobs/SendLeadEmailsJob.php
+++ src/Jobs/SendLeadEmailsJob.php
@@ -71,7 +71,7 @@ class SendLeadEmailsJob implements ShouldQueue
 
                 $lead->update([
                     'email_sent_at' => now(),
-                    'location_email' => $lead
+                    'location_email' => $recipientEmail
                 ]);
             }
         }
