243417bfd4b2885957888cf5c8be74af50988e22
Owen Connor <owen.connor@techquity.co.uk>
2025-02-17 09:48:20 +0000

    Update lead.blade.php

--- resources/views/mail/lead.blade.php
+++ resources/views/mail/lead.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>New Lead</h1>
 
-    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="input ">
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="input">
         <tbody>
         <tr>
             <td align="left">
@@ -18,7 +18,7 @@
                 <label>Customer Email: </label>
             </td>
             <td align="left">
-                {{ $lead->order->email }}
+                {{ $order->email }}
             </td>
         </tr>
         <tr>
@@ -42,7 +42,7 @@
                 <label>Customer Postcode: </label>
             </td>
             <td align="left">
-                {{ $lead->postcode }}
+                {{ $order->shippingAddress->postcode ?? 'N/A' }}
             </td>
         </tr>
         <tr>
@@ -50,17 +50,30 @@
                 <label>Order Reference: </label>
             </td>
             <td align="left">
-                {{ $lead->order->reference }}
+                {{ $order->reference }}
             </td>
         </tr>
-        <tr>
-            <td align="left">
-                <label>Lead Product: </label>
-            </td>
-            <td align="left">
-                {{ $lead->orderItem->name }} (SKU: {{ $lead->orderItem->sku }})
-            </td>
-        </tr>
+        </tbody>
+    </table>
+
+    <h2>Lead Products</h2>
+
+    <table role="presentation" border="0" cellpadding="0" cellspacing="0" class="input">
+        <thead>
+            <tr>
+                <th align="left">Product Name</th>
+                <th align="left">SKU</th>
+                <th align="left">Quantity</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($orderItems as $item)
+                <tr>
+                    <td>{{ $item->name }}</td>
+                    <td>{{ $item->sku }}</td>
+                    <td>{{ $item->quantity }}</td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 @endsection
